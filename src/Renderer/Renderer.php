8bd2d6c25762f25db685a35a88dc6a3c6d44e677
dyszczo <krzysiek@inspirelabs.pl>
2018-09-23 22:33:49 +0200

    better renderer interface

--- src/Renderer/Renderer.php
+++ src/Renderer/Renderer.php
@@ -12,8 +12,6 @@ interface Renderer {
 	 * Set the resolver used to map a template name to a resource the renderer may consume.
 	 *
 	 * @param  Resolver $resolver
-	 *
-	 * @return Resolver
 	 */
 	public function set_resolver( Resolver $resolver );
 
@@ -23,5 +21,5 @@ interface Renderer {
 	 *
 	 * @return string
 	 */
-	public function render( $template, $params );
+	public function render( $template, array $params );
 }
