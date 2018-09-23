b5eaebd8f3f672d3bb2ea6219fd66818e9959ef2
dyszczo <krzysiek@inspirelabs.pl>
2018-09-23 21:02:59 +0200

    theme resolver test

---
+++ tests/unit/Resolver/TestThemeResolver.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class TestThemeResolver extends \PHPUnit\Framework\TestCase
+{
+
+
+    public function testEndpoint()
+    {
+        $resolver = new \WPDesk\View\Resolver\ThemeResolver();
+    }
+}
